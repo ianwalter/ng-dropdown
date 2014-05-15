56ce1efa4e83e931aed31e9d153b6388fe7c259f
Ian Walter <ian@gothenburg.local>
2014-05-15 17:47:52 -0400

    Changing BG color to white

--- dist/js/ng-dropdown.js
+++ dist/js/ng-dropdown.js
@@ -1,5 +1,5 @@
 /**
- * ng-dropdown - v0.0.4 - A simple AngularJS directive to provide dropdown menu functionality!
+ * ng-dropdown - v0.0.5 - A simple AngularJS directive to provide dropdown menu functionality!
  *
  * @author Ian Kennington Walter (http://ianvonwalter.com)
  */
